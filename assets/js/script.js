c99c90b076ba809fba8cd3d2749b7040b5efe3f1
Kirill Machin <131207378+MrKirilchik@users.noreply.github.com>
2023-09-27 19:01:33 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+function CarryInfo(){
+    document.getElementById("img").innerHTML = "<h1>Carry</h1><br><img style='width='1200' height='700'' alt='Img Carry' src='https://krot.info/uploads/posts/2021-03/1614873656_35-p-anime-neko-art-kartinki-41.jpg'>";
+}
+
+function MidInfo(){
+    document.getElementById("img").innerHTML = "<h1>Mid</h1><br><img style='width='1200' height='700'' alt='Img Mid' src='https://gas-kvas.com/uploads/posts/2023-01/1673330640_gas-kvas-com-p-anime-risunki-neko-tyan-11.jpg'>";
+}
+
+function OfflineInfo(){
+    document.getElementById("img").innerHTML = "<h1>Offlane</h1><br><img style='width='1200' height='700'' alt='Img Offlane' src='https://i.pinimg.com/originals/fa/95/26/fa952676f51999608d1a4f2e3d9647af.jpg'>";
+}
+
+function SoftSupportInfo(){
+    document.getElementById("img").innerHTML = "<h1>Soft Support</h1><br><img style='width='1200' height='700'' alt='Img Soft Support' src='https://ip1.anime-pictures.net/direct-images/a3d/a3daf3a100dd270ede98a47b975d7a4f.png?if=ANIME-PICTURES.NET_-_484321-1786x2500-original-takehana+note-single-long+hair-tall+image-blush.png'>";
+}
+
+function HardSupportInfo(){
+    document.getElementById("img").innerHTML = "<h1>Hard Support</h1><br><img style='width='1200' height='700'' alt='Img Hard Support' src='https://i.imgur.com/I47MPpO.jpg'>";
+}
