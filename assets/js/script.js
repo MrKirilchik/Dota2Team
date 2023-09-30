f4241e31afd32da05618a3a3effb0d5dccd02409
Kirill Machin <131207378+MrKirilchik@users.noreply.github.com>
2023-10-01 01:02:07 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 function CarryInfo(){
-    document.getElementById("img").innerHTML = "<h1>Carry</h1><br><img style='width='1200' height='700'' alt='Img Carry' src='images/carry.jpg'>";
+    document.getElementById("img").innerHTML = "<h1>Carry</h1><br><img class='photo'' alt='Img Carry' src='images/carry.jpg'>";
 }
 
 function MidInfo(){
-    document.getElementById("img").innerHTML = "<h1>Mid</h1><br><img style='width='1200' height='700'' alt='Img Mid' src='images/mid.jpg'>";
+    document.getElementById("img").innerHTML = "<h1>Mid</h1><br><img class='photo'' alt='Img Mid' src='images/mid.jpg'>";
 }
 
 function OfflineInfo(){
-    document.getElementById("img").innerHTML = "<h1>Offlane</h1><br><img style='width='1200' height='700'' alt='Img Offlane' src='images/offlane.jpg'>";
+    document.getElementById("img").innerHTML = "<h1>Offlane</h1><br><img class='photo'' alt='Img Offlane' src='images/offlane.jpg'>";
 }
 
 function SoftSupportInfo(){
-    document.getElementById("img").innerHTML = "<h1>Soft Support</h1><br><img style='width='1200' height='700'' alt='Img Soft Support' src='images/ssupport.png'>";
+    document.getElementById("img").innerHTML = "<h1>Soft Support</h1><br><img class='photo'' alt='Img Soft Support' src='images/ssupport.png'>";
 }
 
 function HardSupportInfo(){
-    document.getElementById("img").innerHTML = "<h1>Hard Support</h1><br><img style='width='1200' height='700'' alt='Img Hard Support' src='images/hsupport.jpg'>";
+    document.getElementById("img").innerHTML = "<h1>Hard Support</h1><br><img class='photo'' alt='Img Hard Support' src='images/hsupport.jpg'>";
 }
